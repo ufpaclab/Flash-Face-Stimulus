a6b139a68a46cec97457f4239df272c03929fbc6
Grey Johnson <gjohnson@osisoft.com>
2020-08-25 11:36:09 -0700

    clean up preload urls

--- experiment/Globals.js
+++ experiment/Globals.js
@@ -1,7 +1,7 @@
-const FIXATION = `${REPO_URL}/experiment/img/${FIXATION_NAME}`
+const FIXATION = `experiment/img/${FIXATION_NAME}`
 const TRIALS = Math.min(EXPECTED_TRIALS, IMAGE_NAMES.length/2);
 const FACES = (() => {
     let images = []
-    IMAGE_NAMES.forEach(value => {images.push(`${REPO_URL}/experiment/img/${value}`)})
+    IMAGE_NAMES.forEach(value => {images.push(`experiment/img/${value}`)})
     return images
 })()
