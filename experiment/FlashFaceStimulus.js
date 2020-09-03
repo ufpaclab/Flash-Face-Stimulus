36cc67b4b2eb9c578b1598b8f2e74b1f874b03ea
Grey Johnson <gjohnson@osisoft.com>
2020-09-03 13:34:02 -0400

    increased saved information

--- experiment/FlashFaceStimulus.js
+++ experiment/FlashFaceStimulus.js
@@ -3,18 +3,22 @@ const EXPECTED_TRIALS = 5
 const FACE_NAMES = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg']
 const FIXATION_NAME = 'fixationSameSize.png'
 const IMAGE_DIRECTORY = 'images/'
+const IMAGE_DURATION = 1000
 
 // Derived Globals
 const TRIALS = Math.min(EXPECTED_TRIALS, FACE_NAMES.length/2);
-const FACES = (() => {
-    let images = []
-    FACE_NAMES.forEach(image => {images.push(`${IMAGE_DIRECTORY}/${image}`)})
-    return images
-})()
 const FIXATION = `${IMAGE_DIRECTORY}/${FIXATION_NAME}`
 
+// Utility Funcitons
+
+function ImageNamesToImages(imageNames) {
+    var images = []
+    imageNames.forEach(image => {images.push(`${IMAGE_DIRECTORY}/${image}`)})
+    return images
+}
+
 // Page Definitions
-const WelcomePage = {
+let WelcomePage = {
     type: 'html-keyboard-response',
     stimulus:`
     <p>Welcome to the experiment.</p>
@@ -22,7 +26,7 @@ const WelcomePage = {
     `
 }
 
-const InstructionsAndEnterFullscreenPage = {
+let InstructionsAndEnterFullscreenPage = {
     type: 'fullscreen',
     message: `
     <h1>Instructions</h1>
@@ -30,27 +34,30 @@ const InstructionsAndEnterFullscreenPage = {
     `
 }
 
-const ExitFullscreenPage = {
+let ExitFullscreenPage = {
     type: 'fullscreen',
     fullscreen_mode: false
 }
 
-const ExperimentPage = {
+let ExperimentPage = {
     type: 'html-keyboard-response',
-    trial_duration: 1000,
+    trial_duration: IMAGE_DURATION,
     choices: jsPsych.NO_KEYS,
     timeline: [
         {  
-            stimulus: () => `
+            stimulus: function() {
+                return `
                 <img id="leftImage" src="${jsPsych.timelineVariable('leftFace', true)}">
                 <img id="fixation" src="${FIXATION}">
                 <img id="rightImage" src="${jsPsych.timelineVariable('rightFace', true)}">`
+            }
         }
     ],
-    timeline_variables: (() => {
+    timeline_variables: function() {
         const facesRequired = TRIALS*2;
-        const faces = jsPsych.randomization.sampleWithoutReplacement(FACES, facesRequired)
+        this.faceNames = jsPsych.randomization.sampleWithoutReplacement(FACE_NAMES, facesRequired)
 
+        var faces = ImageNamesToImages(this.faceNames)
         var trialVariables = []
         for(var i = 0; i < faces.length; i+=2) {
             trialVariables.push({
@@ -59,10 +66,11 @@ const ExperimentPage = {
             })
         }
         return trialVariables;
-    })(),
+    }(),
 }
 
-const MeasureDistortionPage = {
+let MeasureDistortionPage = {
+    tags: ['Response'],
     type: 'html-slider-response',
     start: 1,
     min: 1,
@@ -75,27 +83,40 @@ const MeasureDistortionPage = {
 }
 
 // Controller Functions
-const StartExperiment = () => {
+function PreloadImages() {
+    var preloads = ''
+    preloads += `<link rel="preload" href="${FIXATION}" as="image"></link>` 
+    var faces = ImageNamesToImages(FACE_NAMES)
+    faces.forEach((image) => {
+        preloads += `<link rel="preload" href="${image}" as="image"></link>` 
+    })
+    document.write(preloads)
+}
+
+function StartExperiment() {
     jsPsych.init({
         timeline: [WelcomePage, InstructionsAndEnterFullscreenPage, ExperimentPage, ExitFullscreenPage, MeasureDistortionPage],
         on_finish: () => {
             const results = JSON.parse(jsPsych.data.get().json())
+
             var responses = []
-            results.forEach(value => {
-                if (value.trial_type == 'html-slider-response') {
+            var faceNames = results.find((element => element.tags.includes('FlashFace'))).faceNames
+            results.forEach(trial => {
+                if (trial.tags.includes('FlashFace')) {
+                    faceNames = trial.faceNames
+                }
+                if (trial.tags.includes('Response')) {
                     responses.push(value.response)
                 }
             })
-            jsPsychSheet.Insert('Responses', responses)
+            
+            var entry = []
+            entry.push(TRIALS)
+            entry.push(IMAGE_DURATION)
+            entry.push(FACE_NAMES.toString())
+            entry.push(faceNames.toString())
+            entry.push(responses.toString())
+            jsPsychSheet.Insert('Responses', entry)
         }
     })
-}
-
-const PreloadImages = () => {
-    var preloads = ''
-    preloads += `<link rel="preload" href="${FIXATION}" as="image"></link>` 
-    FACES.forEach((image) => {
-        preloads += `<link rel="preload" href="${image}" as="image"></link>` 
-    })
-    document.write(preloads)
 }
